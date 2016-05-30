536c3b9cdaa3ed2ca80a3b0bfe9cc1a734c601be
Christopher Younger <christophernyounger@gmail.com>
2016-05-30 13:23:29 -0500

    finished with age program

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "How old are you?"
+
+person_age = gets.chomp
+
+puts "In 10 years you will be #{person_age.to_i + 10 } years old!"
+
+puts "In 20 years you will be #{person_age.to_i + 20 } years old!"
+
+puts "In 30 years you will be #{person_age.to_i + 30 } years old!"
+
+puts "In 40 years you will be #{person_age.to_i + 40 } years old!"
