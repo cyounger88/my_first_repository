6a345a913219ca603fd7779d7d857fc5d15943c0
Christopher Younger <christophernyounger@gmail.com>
2016-05-31 09:24:43 -0500

    methods complete

---
+++ methodexcercise.rb
@@ -0,0 +1,22 @@
+def greeting(name)
+
+  "hello, " + name + ". How are you doing?"
+
+end
+
+puts greeting("Chris")
+
+
+def multiply(a,b)
+  a*b
+end
+
+p multiply(9,9)
+
+def scream(words)
+  words = words + "!!!!"
+  
+ 
+end
+
+p scream("Yippeee")
